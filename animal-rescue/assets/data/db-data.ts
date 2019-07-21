87cca9d5be450bf39423a570811c072d02c79365
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-07-21 10:21:51 -0400

    Auto-generated commit

---
+++ animal-rescue/assets/data/db-data.ts
@@ -0,0 +1,110 @@
+export const Pets = [
+  {
+    id: 1,
+    species: "Dog",
+    name: "Mocha",
+    breed: "Chocolate Lab",
+    sex: "Female",
+    age: "10 weeks",
+    weight: "10 pounds",
+    img:
+      "https://images.unsplash.com/photo-1447684808650-354ae64db5b8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1347&q=80",
+    alt: "Photo by Jairo Alzate on Unsplash"
+  },
+  {
+    id: 2,
+    species: "Dog",
+    name: "Chase",
+    breed: "Terrier Pit Bull/Mix",
+    sex: "Male",
+    age: "16 weeks",
+    weight: "15 pounds",
+    img:
+      "https://images.unsplash.com/photo-1477884143921-51d0a574ee09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    alt: "Photo by Justin Veenema on Unsplash"
+  },
+  {
+    id: 3,
+    species: "Cat",
+    name: "Lasagna",
+    breed: "Domestic Shorthair",
+    sex: "Male",
+    age: "4 years 10 months",
+    weight: "12 pounds",
+    img:
+      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: "Photo by Erik-Jan Leusink on Unsplash"
+  },
+  {
+    id: 4,
+    species: "Cat",
+    name: "Dreamsicle",
+    breed: "Mediumhair",
+    sex: "Female",
+    age: "1 year 9 months",
+    weight: "10 pounds",
+    img:
+      "https://images.unsplash.com/photo-1545424920-dfef0be9bfc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: "Photo by fotografierende on Unsplash"
+  },
+  {
+    id: 5,
+    species: "Dog",
+    name: "Diesle",
+    breed: "Pit Bull/Mix",
+    sex: "Male",
+    age: "1 year 5 months",
+    weight: "30 pounds",
+    img:
+      "https://images.unsplash.com/photo-1529956650128-bca881be2e60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    alt: "Photo by Edgar Chaparro on Unsplash"
+  },
+  {
+    id: 6,
+    species: "Cat",
+    name: "Noodle",
+    breed: "Domestic Shorthair",
+    sex: "Female",
+    age: "2 years 5 months",
+    weight: "15 pounds",
+    img:
+      "https://images.unsplash.com/photo-1534330980656-d201223895ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    alt: "Photo by Isabela Ferreira on Unsplash"
+  },
+  {
+    id: 7,
+    species: "Cat",
+    name: "Pookie",
+    breed: "Maine Coon",
+    sex: "Female",
+    age: "6 Weeks",
+    weight: "5 pounds",
+    img:
+      "https://images.unsplash.com/photo-1542736143-29a8432162bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: "Photo by Diana Parkhouse on Unsplash"
+  },
+  {
+    id: 8,
+    species: "Cat",
+    name: "Winchester",
+    breed: "Bengal",
+    sex: "Male",
+    age: "3 Years 1 month",
+    weight: "15 pounds",
+    img:
+      "https://images.unsplash.com/photo-1493536122405-24f67df95909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    alt: "Photo by Caleb Woods on Unsplash"
+  },
+  {
+    id: 9,
+    species: "Dog",
+    name: "Coco",
+    breed: "Chocolate Lab",
+    sex: "Female",
+    age: "2 Years",
+    weight: "49 pounds",
+    img:
+      "https://images.unsplash.com/photo-1512251275231-63e355f3d028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    alt: "Photo by Rebekah Howell on Unsplash"
+  }
+];
