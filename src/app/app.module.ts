0c04f88a4dff895aba1d0b4c05f064bbd576657d
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-07-20 20:17:26 -0400

    Add SectionHeader Component. Add section header HTML and styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeroComponent } from './hero/hero.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HeroComponent
+    HeroComponent,
+    SectionHeaderComponent
   ],
   imports: [
     BrowserModule
