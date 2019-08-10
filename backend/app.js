2af17c991bffb9ba11156e88609e71d4c296de0f
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-08-10 09:24:04 -0400

    Add functionality and route to get single pet

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,27 @@ const getAllPets = (req, res) => {
   });
 };
 
+const getPet = (req, res) => {
+  const id = parseInt(req.params.id);
+  const pet = pets.find(pet => pet.id === id);
+
+  if (!pet) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      pet
+    }
+  });
+};
+
 app.route("/api/v1/pets").get(getAllPets);
+app.route("/api/v1/pets/:id").get(getPet);
 
 // SERVER
 const port = 3000;
